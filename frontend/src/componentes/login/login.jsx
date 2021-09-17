b288b89a383c4308cf55a3556b6517abb387809b
unknown <felibarreto2013@gmail.com>
2021-09-17 02:15:03 -0300

    Login hecho como en Figma

--- frontend/src/componentes/login/login.jsx
+++ frontend/src/componentes/login/login.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './login.css'
-import icono from '../../imagenes/icono_login.jpg'
+import icono from '../../imagenes/icono_login.png'
 import "bootstrap/dist/css/bootstrap.min.css"
 
 const Login = () => {
@@ -17,19 +17,21 @@ const Login = () => {
     }
 
     return(
-        <div className="wrapper fadeInDown">
+        <div id="el_wrapper" className="wrapper fadeInDown">
             <div id="formContent">
 
-                <div className="fadeIn first">
+                <div id="contenedor_de_icono" className="fadeIn first">
                     <img src={icono} id="icon" alt="User Icon" />
                 </div>
 
                 <form>
+                    <h3 id="dni_que_no_es_input">DNI</h3>
                     <input type="text" id="dni" className="fadeIn second" name="dni" placeholder="DNI" onChange={capturar_el_ingreso_de_datos}/>
-                    <input type="password" id="password" className="fadeIn third" name="password" placeholder="Password" onChange={capturar_el_ingreso_de_datos}/>
+                    <h3 id="contraseña_que_no_es_input">Contraseña</h3>
+                    <input type="password" id="password" className="fadeIn third" name="password" placeholder="Contraseña" onChange={capturar_el_ingreso_de_datos}/>
                     <br/>
                     <br/>
-                    <input type="submit" className="fadeIn fourth" value="Log In"/>
+                    <input id="boton_inicio_de_sesion" type="submit" className="fadeIn fourth" value="Iniciar sesión"/>
                 </form>
 
                 {false &&
