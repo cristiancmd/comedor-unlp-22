3f8360a5f0a49ced8672360fa0b602d1c1065852
cristian <cristian.cmd@gmail.com>
2021-09-17 23:00:52 -0300

    implementando autenticacion de usuarios mediante tokens

---
+++ backend/comedor/api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    authentication_classes = (TokenAuthentication,)
