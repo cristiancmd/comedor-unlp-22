9f0b85e06786c519d0f3a854c11c514b3d82d946
echeva <yaniiecheva@gmail.com>
2021-09-22 03:31:03 -0300

    Models

--- backend/comedor/api/models.py
+++ backend/comedor/api/models.py
@@ -1,3 +1,85 @@
 from django.db import models
+from django.utils.translation import ugettext as _
+from .validators import *
 
 # Create your models here.
+
+MEASURE = [
+    ("KG", "Kilogramo"),
+    ("GR", "Gramo"),
+    ("UN", "Unidad"),
+    ("LT", "Litro")
+]
+
+CAMPUS = [
+    ("SEDE-I", "Sede I Bosque Oeste: calle 50 entre 116 y 117"),
+    ("SEDE-II", "Sede II Bosque Este: boulevard 120 entre 61 y 62 N° 1439"),
+    ("SEDE-III", "Sede III ATULP: av. 44 Nº 733 entre 9 y 10, Asociación "
+                 "de Trabajadores de la UNLP"),
+    ("SEDE-IV", "Sede IV Club Everton: 14 entre 63 y 64, salón planta baja")
+]
+
+
+class Ingredient(models.Model):
+    name = models.CharField(_("Nombre"), max_length=30, validators=[alphabetical])
+    measure = models.CharField(_("Unidad de medida"), choices=MEASURE)
+
+    class Meta:
+        verbose_name = _("Ingrediente")
+        verbose_name_plural = _("Ingredientes")
+
+    def __unicode__(self):
+        return u"%s" % self.name
+
+    def __str__(self):
+        return self.name
+
+
+class Component(models.Model):
+    name = models.CharField(_("Nombre"), max_length=30, validators=[alphabetical])
+    ingredients = models.ManyToManyField(Ingredient, verbose_name="Ingredientes",
+                                         through="IngredientsWithMeasure")
+
+    class Meta:
+        verbose_name = _("Componente/Plato")
+        verbose_name_plural = _("Componentes/Platos")
+
+    def __str__(self):
+        return self.name
+
+
+class IngredientsWithMeasure(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    component = models.ForeignKey(Component, on_delete=models.CASCADE)
+    amount = models.IntegerField(_("Cantidad"))
+
+    class Meta:
+        verbose_name = "Ingredientes del plato"
+        verbose_name_plural = "Ingredientes en los platos"
+
+    def __str__(self):
+        return f"{self.component.name} lleva {self.amount}{self.ingredient.measure} " \
+               f"de {self.ingredient.name}"
+
+
+class Menu(models.Model):
+    name = models.CharField(_("Nombre"), max_length=30, validators=[alphabetical])
+    starter = models.ManyToManyField(Component, verbose_name="Entrada", related_name="menus")
+    principal = models.ManyToManyField(Component, verbose_name="Plato principal", related_name="menus")
+    dessert = models.ManyToManyField(Component, verbose_name="Postre", related_name="menus")
+    drink = models.ManyToManyField(Component, verbose_name="Bebida", related_name="menus")
+    celiac = models.BooleanField(_("Menú celíaco"), default=False, null=True, blank=True)
+    vegetarian = models.BooleanField(_("Menú vegetariano"), default=False, null=True, blank=True)
+    image = models.ImageField(_("Foto del menú"), blank=True, null=True, upload_to='menus')
+    enabled = models.BooleanField(_("Habilitado"), default=True)
+    # campus = models.CharField(_("Sedes"), max_length=20, choices=CAMPUS)
+    # dates = models.ManyToManyField(datetime, "Días", null=True)
+    servings = models.IntegerField(_("Porciones"))
+    price = models.FloatField(_("Precio"))
+
+    class Meta:
+        verbose_name = _("Menú")
+        verbose_name_plural = _("Menús")
+
+    def __str__(self):
+        return self.name
---
+++ backend/comedor/api/validators.py
@@ -0,0 +1,16 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+import datetime
+
+from django.core.validators import RegexValidator
+
+alphabetical = RegexValidator(
+    r'^[a-zA-ZÀ-ÿ\u00f1\u00d1\'\s\-]+$', 'Este campo solo acepta caracteres alfabéticos.')
+
+
+def date_is_valid(value):
+    try:
+        if value <= datetime.date.today():
+            raise ValidationError("La fecha del ticket debe ser mayor al día de hoy")
+    except Exception as e:
+        raise ValidationError("La fecha del ticket debe ser mayor al día de hoy")
