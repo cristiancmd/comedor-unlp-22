d611633b085c541eba3a0ab57ca27e772f1bd96b
unknown <felibarreto2013@gmail.com>
2021-09-30 00:30:30 -0300

    Listado de platos

--- frontend/src/componentes/header/header.jsx
+++ frontend/src/componentes/header/header.jsx
@@ -12,6 +12,7 @@ const Header = () => {
             <div id="contenedor_de_barra_de_navegacion_header">
                 <Link to={"/home"}><img src={icono_home} id="icono_home"/></Link>
                 <Link to={"/listado_de_menus"}><h5 className="boton">Menús</h5></Link>
+                <Link to={"/listado_de_componentes"}><h5 className="boton">Platos</h5></Link>
                 <Link to={"/listados"}><h5 className="boton">Listados</h5></Link>
                 <div id="contenedor_cerrar_sesion">
                     <button type="button" className="btn btn-outline-light" id="cerrar_sesion"><h5>Cerrar sesión</h5></button>
---
+++ frontend/src/componentes/listado_de_componentes/listado_de_componentes.jsx
@@ -0,0 +1,113 @@
+import './listado_de_componentes.css'
+import React from 'react'
+import axios from "axios"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap'
+import { Link } from 'react-router-dom'
+import Header from '../header/header'
+
+const Listado_de_componentes = () => {
+
+  const url = "http://localhost:8000/api/components/";
+
+  const [data, setData] = React.useState([])
+  const [nuevoComponente, setNuevoComponente] = React.useState({
+    pk:"",
+    name:"",
+  })
+  const [modalEliminar, setModalEliminar] = React.useState(false)
+
+  React.useEffect(() => {
+    peticionGet()
+  }, [])
+
+  const peticionGet = () => {
+    axios.get(url).then(response => {
+      setData(response.data);
+    }).catch(error=>{
+      console.log(error.message);
+    })
+  }
+
+  const seleccionarComponente = (componente) => {
+    setNuevoComponente({
+      pk:componente.pk,
+      name:componente.name,
+    });
+  }
+
+  const peticionDelete = () => {
+    axios.delete(url+"/"+nuevoComponente.pk).then(response=>{
+      setModalEliminar(false);
+      peticionGet();
+    }).catch(error=>{
+      console.log(error.message);
+    })
+  }
+
+  const unidadDeIngrediente = (ingrediente) => {
+    if (ingrediente.ingredient.measure === "UN") {
+      if (ingrediente.amount === 1) {
+        return "Unidad"
+      }
+      else {
+        return "Unidades"
+      }
+    }
+    return ingrediente.ingredient.measure
+  }
+
+  return (
+    <>
+      {Header()}
+      <div id="contenedor_listado_de_componentes" className="App">
+        <br/><br/>
+        <Link to={"/cargar_componente"}><button className="btn btn-primary">Cargar plato</button></Link>
+        <br/><br/>
+        <h2>Platos</h2>
+        <br/>
+        <table className="table">
+          <thead>
+            <tr id="lista_de_titulos_de_columnas_de_componentes">
+              <th className="titulo_de_columna_de_componentes">Nombre</th>
+              <th className="titulo_de_columna_de_componentes">Ingredientes</th>
+              <th className="titulo_de_columna_de_componentes">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map(componente => {
+              return (
+                <tr>
+                  <td>{componente.name}</td>
+                  <td>{componente.ingredients.map(ingrediente => {
+                    return (
+                      <h6>{ingrediente.ingredient.name} {ingrediente.amount} {unidadDeIngrediente(ingrediente)}</h6>
+                    )
+                  })}</td>
+                  <td>
+                    <Link to={"/detalle_componente/"+componente.pk}><button className="btn btn-primary">Detalle</button></Link>
+                    {"   "}
+                    <button className="btn btn-danger" onClick={()=>{seleccionarComponente(componente); setModalEliminar(true)}}>Eliminar</button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+
+        <Modal isOpen={modalEliminar}>
+          <ModalBody>
+            ¿Estás seguro que querés eliminar el plato: {nuevoComponente && nuevoComponente.name}?
+          </ModalBody>
+          <ModalFooter>
+            <button className="btn btn-success" onClick={()=>peticionDelete()}>Sí</button>
+            <button className="btn btn-danger" onClick={()=>setModalEliminar(false)}>No</button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    </>
+  )
+
+}
+
+export default Listado_de_componentes
