0a8de7ec6a387c954b6071bc1a14f2ede1470df2
cristian <cristian.cmd@gmail.com>
2021-10-07 18:48:21 -0300

    vista para campuses en backend

--- frontend/src/componentes/menus_habilitados/menus_habilitados.jsx
+++ frontend/src/componentes/menus_habilitados/menus_habilitados.jsx
@@ -6,21 +6,7 @@ import {Breadcrumb, BreadcrumbItem} from "reactstrap";
 
 const Menus_habilitados = () => {
 
-    const url = "http://localhost:4000/menus"
-
-    const [data, setData] = React.useState([])
-
-    React.useEffect(() => {
-        peticionGet()
-    }, [])
-    
-    const peticionGet = () => {
-        axios.get(url).then(response => {
-            setData(response.data);
-        }).catch(error=>{
-            console.log(error.message);
-        })
-    }
+    const url = "http://localhost:8000/api/enabledmenus"
 
     const fecha_actual = () => {
         let hoy = new Date()
@@ -33,8 +19,20 @@ const Menus_habilitados = () => {
         hoy = año + "-" + mes + "-" + dia
         return hoy
     }
-
+    const [data, setData] = React.useState([])
     const [fecha, setFecha] = React.useState(fecha_actual());
+
+    
+    
+    const peticionGet = () => {
+        axios.get(url).then(response => {
+            setData(response.data);
+        }).catch(error=>{
+            console.log(error.message);
+        })
+    }
+
+
 
     const capturar_el_ingreso_de_fecha = async f => {
         f.persist();
@@ -42,43 +40,11 @@ const Menus_habilitados = () => {
         console.log(fecha);
     }
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    // CUANDO ESTÉ LA API DE VERDAD SUPONGO QUE HABRÍA QUE HACER LOS SIGUIENTES CAMBIOS:
-
-    // 1) CAMBIAR LA VARIABLE url, PARA QUE QUEDE DE FORMA TAL QUE AL AGREGARLE /una_fecha TE DE LOS MENÚS DE ESA FECHA.
-
-    // 2) CAMBIAR peticionGet HACIENDO QUE LE AGREGUE LA FECHA QUE ESTÁ EN EL ESTADO fecha. COMO fecha SE INICIALIZA CON
-    // LA FECHA ACTUAL, PRIMERO BUSCA LOS MENÚS DE HOY, PERO CUANDO SE CAMBIA fecha, BUSCA LOS DE ESA FECHA.
-    // SUPONGO QUE QUEDARÍA ALGO ASÍ:
-
-    // const peticionGet = () => {
-    //     axios.get(url+"/"+fecha).then(response => {
-    //         setData(response.data);
-    //     }).catch(error=>{
-    //         console.log(error.message);
-    //     })
-    // }
-
-    // PUEDE QUE HAYA QUE MOVER peticionGet DE LUGAR PORQUE POR AHÍ NO PUEDE USAR fecha PORQUE ESTÁ DECLARADO MÁS ABAJO.
-    // TAMBIÉN HABRÍA QUE VER QUÉ PASA CUANDO SE TOCA LA CRUZ DONDE SE PONE LA FECHA, PORQUE AHÍ QUEDA fecha EN "empty 
-    // string" Y PODRÍA LLEGAR A EXPLOTAR, Y HABRÍA QUE HACER ALGÚN CAMBIO BOLUDO PARA QUE NO EXPLOTE.
-
-    // 3) HACER UN LLAMADO A peticionGet EN capturar_el_ingreso_de_fecha PARA QUE TRAIGA LOS MENÚS HABILITADOS EN LA FECHA
-    // SELECCIONADA.
-    // SUPONGO QUE QUEDARÍA ALGO ASÍ:
-
-    // const capturar_el_ingreso_de_fecha = async f => {
-    //     f.persist();
-    //     await setFecha(f.target.value);
-    //     peticionGet();
-    //     console.log(fecha);
-    // }
-
-    // 4) CAMBIAR 5===5, 5===6 Y Sede POR ALGO DEL ESTILO menu.vegetariano, menu.caliaco Y menu.sede.
-
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+    React.useEffect(() => {
+        peticionGet()
+        console.log(data)
+    }, [fecha])
+    
     return (
         <>
             {Header()}
@@ -88,9 +54,11 @@ const Menus_habilitados = () => {
                     <BreadcrumbItem active tag="span">Home</BreadcrumbItem>
                   </Breadcrumb>
                 </div>
-            <h1 id="titulo_menus_habilitados">Menús habilitados</h1>
+            <h1 id="titulo_menus_habilitados">Menús habilitados</h1>    
             <h3 id="elegir_fecha_menus_habilitados">Elegir fecha</h3>
-            <input className="form-control" type="date" name="fecha" id="fecha_menus_habilitados" defaultValue={fecha_actual()} onChange={capturar_el_ingreso_de_fecha}/>
+            <div className="container" ><input type="date" name="fecha" 
+            defaultValue={fecha_actual()} onChange={capturar_el_ingreso_de_fecha}/>
+            </div>
             <div id="contenedor_menus_habilitados">
                 <table className="table">
                     <thead>
@@ -98,25 +66,29 @@ const Menus_habilitados = () => {
                             <th className="titulo_de_columna_menus_habilitados">Nombre</th>
                             <th className="titulo_de_columna_menus_habilitados">Vegetariano</th>
                             <th className="titulo_de_columna_menus_habilitados">Celíaco</th>
+                            
+                            <th className="titulo_de_columna_menus_habilitados">Porciones</th>
                             <th className="titulo_de_columna_menus_habilitados">Sede</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {data.map(menu => {
+                        {data.map(data => {
                             return (
-                                <tr>
-                                    <td>{menu.nombre}</td>
+                                <tr >
+                                    <td >{data.menu.name}</td>
                                     {
-                                        5 === 5?
+                                        data.menu.vegetarian?
                                         <td><span className="tilde_vegetariano_menus_habilitados">&#10003;</span></td>:
                                         <td><span className="tilde_vegetariano_menus_habilitados">&#x2715;</span></td>
                                     }
                                     {
-                                        5 === 6?
+                                        data.menu.celiac?
                                         <td><span className="tilde_celiaco_menus_habilitados">&#10003;</span></td>:
                                         <td><span className="tilde_celiaco_menus_habilitados">&#x2715;</span></td>
                                     }
-                                    <td>Sede</td>
+                                    <td >{data.servings }</td>
+                                    <td >{data.campus.name +' - '+ data.campus.address}</td>
+                                    
                                 </tr>
                             )
                         })}
